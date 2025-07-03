10e662855f533eae24713d6baa5d65e608fada33
iqratariqq <120326978+iqratariqq@users.noreply.github.com>
2025-07-04 01:23:04 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const path=require("path");
-
-
+const path=require("path")
 
 //dotenv configuaration
 dotenv.config();
 
 //res object
 const app = express();
-
-const __dirname=path.resolve();
 
 //middleware
 app.use(cors());
@@ -26,11 +22,18 @@ app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'./client/build/index.html'))
 })
 
+// console.log(
+//   app._router.stack
+//     .filter(r => r.route)
+//     .map(r => r.route.path)
+// );
+
+
 //port
 
 const PORT = process.env.PORT || 5000;
 
 //listning
 app.listen(PORT,()=>{
-    console.log(`node server is running at ${PORT}`)
+    console.log(`node server is running at http://localhost:${PORT}`)
 })
